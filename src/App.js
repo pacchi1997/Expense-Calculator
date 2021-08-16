01e7aa98f233d981810163c1b01a6f7c979e144b
prashanth kumar <32808287+pacchi1997@users.noreply.github.com>
2021-08-16 23:28:10 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,48 @@
+import React,{useState} from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Expenses from "./components/ExpenseFolder/Expenses";
+import NewExpense from "./components/NewExpenseFolder/NewExpense";
 
+const dummyExpenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+function App() {
+  
+  const [expenses,setExpenses]= useState(dummyExpenses);
+
+  const newExpenseDataHandler = (newExpenseData) => {
+ 
+    setExpenses((prevState)=>{
+      return [newExpenseData,...prevState];
+    })
+     console.log(expenses);
+  };
+
+  return (
+    <div className="App">
+      <NewExpense onNewExpenseData={newExpenseDataHandler} />
+      <Expenses expensesProps={expenses} />
+    </div>
+  );
+}
+
+export default App;
